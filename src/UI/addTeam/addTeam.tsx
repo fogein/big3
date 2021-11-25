379386c4ffde7b18a3d2a3bdc68115fc75e106fa
Fogein <fogein3311@gmail.com>
2021-11-25 14:02:50 +0300

    complete inputs

--- src/UI/addTeam/addTeam.tsx
+++ src/UI/addTeam/addTeam.tsx
@@ -1,9 +1,20 @@
 
+import { useForm } from "react-hook-form";
 import { Link } from 'react-router-dom'
 import addPhotoTeam from '../../assets/images/addPhotoTeam.svg'
 import cls from './addTeam.module.scss'
 
 export const AddTeam = () => {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit
+  } = useForm({
+    mode: "onChange"
+  });
+  const onSubmit = (data:any) => {
+    console.log(data);
+  };
   return (
       <div className={cls.mainContainer}  >
       <div className={cls.topConttainer}>
@@ -15,12 +26,48 @@ export const AddTeam = () => {
           <img src={addPhotoTeam} alt="" />
         </button>
         </div>
-        <form className={cls.inputsContainer}>
-          <input className={cls.addTeamInput} type="text" />
-          <input className={cls.addTeamInput} type="text" />
-          <input className={cls.addTeamInput} type="text" />
-          <input className={cls.addTeamInput} type="text" />
-        </form>
+              <form className={cls.inputsContainer} onSubmit={handleSubmit(onSubmit)}>
+            <label htmlFor="firstName">First Name</label>
+            <input
+              placeholder="Bill"
+              {...register("firstName", {
+                required: "this is a required",
+                maxLength: {
+                  value: 2,
+                  message: "Max length is 2"
+                }
+              })}
+            />
+            {errors.firstName && <p>{errors.firstName.message}</p>}
+
+            <label htmlFor="lastName">Last Name</label>
+            <input
+              placeholder="Luo"
+              {...register("lastName", {
+                required: "this is required",
+                minLength: {
+                  value: 2,
+                  message: "Min length is 2"
+                }
+              })}
+            />
+            {errors.lastName && <p>{errors.lastName.message}</p>}
+
+            <label htmlFor="email">Email</label>
+            <input
+              placeholder="bluebill1049@hotmail.com"
+              type="text"
+              {...register("email", {
+                required: "this is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+                  message: "Invalid email address"
+                }
+              })}
+            />
+            {errors.email && <p>{errors.email.message}</p>}
+            <input type="submit" />
+          </form>
       </div>
     </div>
   )
